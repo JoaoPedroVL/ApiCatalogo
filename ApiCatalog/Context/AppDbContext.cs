b8eeee99bd254beff05ea811d7903da879f86718
wizar <wizar@DESKTOP-KJ52JKV>
2024-12-18 07:40:54 -0300

    Adicionar arquivos de projeto.

---
+++ ApiCatalog/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ApiCatalog.Model;
+
+namespace ApiCatalog.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Categoria>? Categorias { get; set; }
+    public DbSet<Produtos>? Produtos { get; set; }
+
+
+}
